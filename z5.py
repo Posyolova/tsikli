a6da130a1949f714b21749fe1db7f467d320a96e
Posyolova <143779118+Posyolova@users.noreply.github.com>
2023-12-17 17:33:50 +0700

    Update z5.py

--- z5.py
+++ z5.py
@@ -1,16 +1,1 @@
-n = int(input('введите число-  '))
 
-r = [0, 1]
-
-for i in range(n+1):
-    if n <= 1:
-            break
-    elif i == 0:
-            continue
-
-    r.append(r[i-1]+r[i])
-
-if n > 1:
-    r.pop(-1)
-
-print(r)
